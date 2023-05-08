e7aff3adef6e277c05d5003a16eca975fba115ff
Felipe <andrade.adsf@gmail.com>
2023-05-08 19:29:15 -0300

    working with gnl

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,23 +6,27 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 23:39:58 by feandrad          #+#    #+#             */
-/*   Updated: 2023/05/08 19:07:22 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:27:02 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "../libs/get_next_line/get_next_line.h"
 
 int main(void)
 {
-	int BUFFER_SIZE = 100;
 	int fd;
-	char buffer[BUFFER_SIZE];
 	char *path;
+	char *line;
 
-	path = "../resources/maps/first_map.ber";
+	path = "./resources/maps/first_map.ber";
 	fd = open(path, O_RDONLY);
-	read(fd, buffer, BUFFER_SIZE);
-	printf("%s", buffer);
+	line = get_next_line(fd);
+	while(line != NULL)
+	{
+		printf("%s", line);
+		line = get_next_line(fd);
+	}
 }
