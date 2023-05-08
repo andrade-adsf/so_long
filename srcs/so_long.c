a6d5435c48b97f0697fc54925ab88406fd99c992
Felipe <andrade.adsf@gmail.com>
2023-05-08 20:06:53 -0300

    accepting arguments and validation of arguments

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 23:39:58 by feandrad          #+#    #+#             */
-/*   Updated: 2023/05/08 19:51:23 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/05/08 20:04:13 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,26 @@
 #include <fcntl.h>
 #include "../libs/get_next_line/get_next_line.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
-	int fd;
-	char *path;
-	char *line;
+	int	fd;
+	char	*path;
+	char	*line;
 
-	path = "../resources/maps/first_map.ber";
+	if (argc != 2)
+	{
+		printf("Error\nWrong number of parameters!");
+		return (-1);
+	}
+	path = argv[1];
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 	{
-		printf("Error \nPath not valid!");
+		printf("Error\nPath not valid!");
 		return (-1);
 	}
 	line = get_next_line(fd);
-	while(line != NULL)
+	while (line != NULL)
 	{
 		printf("%s", line);
 		line = get_next_line(fd);
