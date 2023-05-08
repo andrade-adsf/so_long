1897f3be4b010376a6513c3d8fa68a117b58dcc1
Felipe <andrade.adsf@gmail.com>
2023-05-08 19:51:49 -0300

    makefile and path validation

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -Wall -Wextra -Werror ./libs/get_next_line/get_next_line.c ./libs/get_next_line/get_next_line_utils.c ./srcs/so_long.c
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 23:39:58 by feandrad          #+#    #+#             */
-/*   Updated: 2023/05/08 19:27:02 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:51:23 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,18 @@ int main(void)
 	char *path;
 	char *line;
 
-	path = "./resources/maps/first_map.ber";
+	path = "../resources/maps/first_map.ber";
 	fd = open(path, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error \nPath not valid!");
+		return (-1);
+	}
 	line = get_next_line(fd);
 	while(line != NULL)
 	{
 		printf("%s", line);
 		line = get_next_line(fd);
 	}
+	return (0);
 }
