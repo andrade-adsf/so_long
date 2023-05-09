788d94f9a9f234d16bd26d4af502d56ea2da4ddc
Felipe <andrade.adsf@gmail.com>
2023-05-09 19:59:23 -0300

    WIP - saving each map line into a string, need to work on split it into an array

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 23:39:58 by feandrad          #+#    #+#             */
-/*   Updated: 2023/05/09 19:11:39 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:56:13 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@ int main(int argc, char** argv)
 {
 	int	fd;
 	char	*line;
+	char	*aux;
+	char	*linear_map;
 
 	if (input_validation(argc, argv, &fd) == -1)
 		return (-1);
 	line = get_next_line(fd);
+	aux = ft_strdup("");
 	while (line != NULL)
 	{
-		printf("%s", line);
+		linear_map = ft_strjoin(aux, line);
 		free(line);
+		free(aux);
+		aux = linear_map;
 		line = get_next_line(fd);
 	}
+	printf("%s", linear_map);
+	free(linear_map);
 	return (0);
 }
