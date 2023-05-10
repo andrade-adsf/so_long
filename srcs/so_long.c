07aaafc90204f22fe056215a843f875715633f2e
Felipe <andrade.adsf@gmail.com>
2023-05-10 18:41:12 -0300

    printing array map, no leak

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 23:39:58 by feandrad          #+#    #+#             */
-/*   Updated: 2023/05/10 17:55:23 by feandrad         ###   ########.fr       */
+/*   Updated: 2023/05/10 18:39:49 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int main(int argc, char** argv)
 {
 	int	fd;
+	int	map_size;
+	int i;
 	char	*line;
 	char	*aux;
 	char	*linear_map;
@@ -23,6 +25,7 @@ int main(int argc, char** argv)
 	if (input_validation(argc, argv, &fd) == -1)
 		return (-1);
 	line = get_next_line(fd);
+	map_size = 0;
 	aux = ft_strdup("");
 	while (line != NULL)
 	{
@@ -31,10 +34,19 @@ int main(int argc, char** argv)
 		free(aux);
 		aux = linear_map;
 		line = get_next_line(fd);
+		map_size++;
 	}
 	map = ft_split(linear_map, '\n');
 	free(linear_map);
-	printf("%s", map);
+	i = 0;
+	while(i < map_size)
+		{
+			printf("%s", map[i]);
+			if(i < map_size - 1)
+				printf("\n");
+			free(map[i]);
+			i++;
+		}
 	free(map);
 	return (0);
 }
